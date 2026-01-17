989b8b235bd6f9ce0c6e5f30f905468217523c54
Olmosbek Rustamov <o.rustamov@newuu.uz>
2026-01-17 11:56:54 +0500

    7th problem solved

---
+++ 7-misol.js
@@ -0,0 +1,19 @@
+function moveZeroes(nums) {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+
+  while (index < nums.length) {
+    nums[index] = 0;
+    index++;
+  }
+  return nums;
+}
+
+const nums = [0,1,0,3,12];
+console.log(moveZeroes(nums));
